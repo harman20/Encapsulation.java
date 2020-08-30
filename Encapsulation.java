dfe62ad57a27a9daf81b7d667c8c06b048d44bb6
Harmanjit Singh <57390891+harman20@users.noreply.github.com>
2020-08-30 15:51:20 +0530

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,16 @@
+public class Encapsulation {
+private final String email;
+private String address;
+public Encapsulation( final String email ) {
+this.email = email;
+}
+public String getAddress() {
+return address;
+}
+public void setAddress(String address) {
+this.address = address;
+}
+public String getEmail() {
+return email;
+}
+}
